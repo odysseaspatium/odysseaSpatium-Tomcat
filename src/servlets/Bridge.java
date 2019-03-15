823d8996f9bdda9cb3c51bf8435a5cc6284d3112
amulus <axel.murat@laposte.Net>
2019-03-15 18:24:58 +0100

    ListeFichier array

--- src/servlets/Bridge.java
+++ src/servlets/Bridge.java
@@ -1,12 +1,15 @@
 package servlets;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -16,6 +19,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
 /**
@@ -64,7 +68,23 @@ public class Bridge extends HttpServlet {
 		
 		BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
 		JsonNode jsonOut = mapper.readTree(in);
-		
+		String dossier = jsonOut.get("").toString();
+		if(dossier != null) {
+	        File directory = new File(dossier);
+	        List<String> listeFile = new ArrayList<String>();
+	        //get all the files from a directory
+
+	        File[] fList = directory.listFiles();
+
+	        for (File file : fList){
+	            if (file.isFile()){
+	            	listeFile.add(file.getName());
+	            }
+	        }
+	        ArrayNode array = mapper.valueToTree(listeFile);
+	        ((ObjectNode) jsonOut).putArray("fichiers").addAll(array);
+	        System.out.println(jsonOut.toString());
+		}
 		PrintWriter out = response.getWriter();
 		out.write(jsonOut.toString());
 		
